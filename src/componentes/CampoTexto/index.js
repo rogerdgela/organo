d77f2df043b5504a79616d3947d17a7a171391e7
Rogerio Silva <dgelask8@gmail.com>
2024-10-12 09:03:36 -0300

    trabalhando com listas

--- src/componentes/CampoTexto/index.js
+++ src/componentes/CampoTexto/index.js
@@ -3,10 +3,15 @@ import "./CampoTexto.css"
 const CampoTexto = (props) => {
     const placeholderModificado = `${props.placeholder}...`
 
+    const aoDigitado = (evento) => {
+        let valor = evento.target.value
+        console.log(valor)
+    }
+
     return (
         <div className="campo-texto">
             <label>{ props.label }</label>
-            <input required={props.obrigatorio} placeholder={ placeholderModificado } />
+            <input required={props.obrigatorio} onChange={aoDigitado} placeholder={ placeholderModificado } />
         </div>
     )
 }
