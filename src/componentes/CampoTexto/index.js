2708a4b5bd91dd044277e2ea769de7a10349c13a
Rogerio Silva <dgelask8@gmail.com>
2024-10-13 09:00:23 -0300

    Controlando o estado

--- src/componentes/CampoTexto/index.js
+++ src/componentes/CampoTexto/index.js
@@ -1,17 +1,19 @@
+import { useState } from "react"
 import "./CampoTexto.css"
 
 const CampoTexto = (props) => {
     const placeholderModificado = `${props.placeholder}...`
 
+    // let valor = "Rogerio"
+
     const aoDigitado = (evento) => {
-        let valor = evento.target.value
-        console.log(valor)
+        props.aoAlterado(evento.target.value)
     }
 
     return (
         <div className="campo-texto">
             <label>{ props.label }</label>
-            <input required={props.obrigatorio} onChange={aoDigitado} placeholder={ placeholderModificado } />
+            <input value={props.valor} required={props.obrigatorio} onChange={aoDigitado} placeholder={ placeholderModificado } />
         </div>
     )
 }
