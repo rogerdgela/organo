417137c115914d12f83a9e20f2bd516d122b6f24
Rogerio Silva <dgelask8@gmail.com>
2024-10-19 09:07:09 -0300

    Escondendo times vazios

--- src/componentes/Time/index.js
+++ src/componentes/Time/index.js
@@ -5,6 +5,7 @@ const Time = (props) => {
     const estiloBg = { backgroundColor: props.corSecundaria }
     const estiloBorda = { borderColor: props.corPrimaria }
     return (
+        (props.colaboradores.length > 0) ? 
         <section className="time" style={estiloBg}>
             <h3 style={estiloBorda}>{props.nome}</h3>
             <div className="colaboradores">
@@ -12,8 +13,8 @@ const Time = (props) => {
                     props.colaboradores.map(colaborador => <Colaborador nome={colaborador.nome} cargo={colaborador.cargo} imagem={colaborador.imagem} />)
                 }
             </div>
-            
         </section>
+        : ""
     )
 }
 
