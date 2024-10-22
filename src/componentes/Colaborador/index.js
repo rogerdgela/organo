2dd2660384e1528aff68fd7b6e3fffe67d8985cf
Rogerio Silva <dgelask8@gmail.com>
2024-10-22 20:41:50 -0300

    Importando ícone de deletar

--- src/componentes/Colaborador/index.js
+++ src/componentes/Colaborador/index.js
@@ -1,15 +1,19 @@
-import './colaborador.css'
+import './colaborador.css';
+import { AiFillCloseCircle } from "react-icons/ai";
 
-const Colaborador = ({ colaborador, corDeFundo }) => {
-    return (<div className="colaborador">
-        <div className="cabecalho" style={{ backgroundColor: corDeFundo }}>
-            <img src={colaborador.imagem} alt={colaborador.nome} />
+const Colaborador = ({ colaborador, corDeFundo, aoDeletar }) => {
+    return (
+        <div className="colaborador" onClick={aoDeletar}>
+            <AiFillCloseCircle size={25} className="deletar" onClick={aoDeletar} />
+            <div className="cabecalho" style={{ backgroundColor: corDeFundo }}>
+                <img src={colaborador.imagem} alt={colaborador.nome} />
+            </div>
+            <div className="rodape">
+                <h4>{colaborador.nome}</h4>
+                <h5>{colaborador.cargo}</h5>
+            </div>
         </div>
-        <div className="rodape">
-            <h4>{colaborador.nome}</h4>
-            <h5>{colaborador.cargo}</h5>
-        </div>
-    </div>)
+    )
 }
 
 export default Colaborador
