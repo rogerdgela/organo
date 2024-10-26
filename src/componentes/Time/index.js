e667723f5bf927d79a9fc65ba398204039c67c4a
Rogerio Silva <dgelask8@gmail.com>
2024-10-26 08:23:49 -0300

    Criando input de cor

--- src/componentes/Time/index.js
+++ src/componentes/Time/index.js
@@ -3,12 +3,13 @@ import './time.css'
 
 const Time = ({ time, colaboradores, aoDeletar }) => {
     return (
-
-        colaboradores.length > 0 && <section className='time' style={{ backgroundImage: 'url(/imagens/fundo.png)', backgroundColor: time.corPrimaria }}>
+        colaboradores.length > 0 && 
+        <section className='time' style={{ backgroundImage: 'url(/imagens/fundo.png)', backgroundColor: time.corPrimaria }}>
+            <input type='color' className='input-cor' value={time.corSecundaria} />
             <h3 style={{ borderColor: time.corSecundaria }}>{time.nome}</h3>
             <div className='colaboradores'>
-                {colaboradores.map((colaborador, indice) => {                    
-                    return <Colaborador key={indice} colaborador={colaborador} corDeFundo={time.corSecundaria} aoDeletar={aoDeletar}/>
+                {colaboradores.map((colaborador, indice) => {
+                    return <Colaborador key={indice} colaborador={colaborador} corDeFundo={time.corSecundaria} aoDeletar={aoDeletar} />
                 })}
             </div>
         </section>
